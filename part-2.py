42ebf8d520a61017d2e351988343906583bd572d
Jeslyn Lai <imlyj5@gmail.com>
2025-05-05 10:42:15 -0700

    fix digit_match

--- part-2.py
+++ part-2.py
@@ -29,15 +29,19 @@ def is_palindrome(text):
 def digit_match(num1, num2):
     if num1 == 0 and num2 == 0:
         return 1
-
+        
     if num1 == 0 or num2 == 0:
         return 0
-
-    if num1 % 10 == num2 % 10:
+    
+    if (num1 % 10) == (num2 % 10):
         match = 1
     else:
         match = 0
-
-    return match + digit_match(num1 // 10, num2 // 10)
-
-
+    
+    next1 = num1 // 10
+    next2 = num2 // 10
+    
+    if next1 == 0 or next2 == 0:
+        return match
+        
+    return match + digit_match(next1, next2)
