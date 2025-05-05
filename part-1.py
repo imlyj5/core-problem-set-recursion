fee484a56ef6dded0bdb17616e72345431db08a6
Jeslyn Lai <imlyj5@gmail.com>
2025-05-05 09:09:46 -0700

    finished part1

--- part-1.py
+++ part-1.py
@@ -4,17 +4,40 @@
 # the appropriate comment.
 
 # factorial
-
+def factorial(n):
+    if n<0:
+        raise ValueError
+    if n <= 1:
+        return 1
+    return n * factorial(n-1)
 
 
 # reverse
-
+def reverse(text):
+    if not text:
+        return ""
+    if len(text) ==1:
+        return text
+    return text[-1]+reverse(text[:-1])
 
 
 # bunny
+def bunny(count):
+    if count == 1:
+        return 2
+    if count == 0:
+        return 0
+    return 2 + bunny(count-1)
 
 
 
 # is_nested_parens
+def is_nested_parens(parens):
+    if not parens:
+        return True
+        
+    if parens[0] == "(" and parens[-1] == ")":
+        return is_nested_parens(parens[1:-1])
+    return False
 
 
--- part-2.py
+++ part-2.py
@@ -4,13 +4,40 @@
 # the appropriate comment.
 
 # search
-
+def search(array,query):
+    if not array:
+        return False
+    if query != array[0]:
+        return search(array[1:],query)
+    return True
 
 
 # is_palindrome
+def is_palindrome(text):
+    if not text:
+        return False
+    if len(text) ==1:
+        return True
+        
+    if text[0] == text[-1]:
+        return is_palindrome(text[1:-1])
+    return False
 
 
 
 # digit_match
+def digit_match(num1, num2):
+    if num1 == 0 and num2 == 0:
+        return 1
+
+    if num1 == 0 or num2 == 0:
+        return 0
+
+    if num1 % 10 == num2 % 10:
+        match = 1
+    else:
+        match = 0
+
+    return match + digit_match(num1 // 10, num2 // 10)
 
 
